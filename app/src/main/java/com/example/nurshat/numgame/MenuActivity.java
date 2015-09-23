0320a1d22a0e5c64886d5ee25862d6fb7c541e4b
Nurshat Nizamov <q7ace@yandex.ru>
2015-09-23 23:38:39 +0300

    add menu

---
+++ app/src/main/java/com/example/nurshat/numgame/MenuActivity.java
@@ -0,0 +1,54 @@
+package com.example.nurshat.numgame;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MenuActivity extends ActionBarActivity {
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+
+    }
+    public void clickListener(View v){
+        switch (v.getId()){
+            case R.id.play:
+                intent = new Intent(MenuActivity.this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.statistic:
+                System.out.println("stat");
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
